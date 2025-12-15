eaefc2e1848cc89f87e5b420b4561205b073477a
lapointek <lapointek.dev@gmail.com>
2025-12-15 15:30:35 -0500

    refactor: extract logic into map_data function

--- src/main.rs
+++ src/main.rs
@@ -34,7 +34,7 @@ fn main() {
     if let Ok(does_exist) = fs::exists(&path) {
         if does_exist {
             for file in get_files(&path) {
-                println!("{}", file)
+                println!("{:?}", file)
             }
         } else {
             println!("{}", "Path does not exist".red());
@@ -44,18 +44,32 @@ fn main() {
     }
 }
 
-fn get_files(path: &Path) -> Vec<String> {
+fn get_files(path: &Path) -> Vec<FileEntry> {
     let mut data = Vec::default();
     if let Ok(read_dir) = fs::read_dir(path) {
         for entry in read_dir {
             if let Ok(file) = entry {
-                data.push(
-                    file.file_name()
-                        .into_string()
-                        .unwrap_or("unknown name".into()),
-                )
+                map_data(file, &mut data);
             }
         }
     }
     data
+}
+
+fn map_data(file: fs::DirEntry, data: &mut Vec<FileEntry>) {
+    if let Ok(meta) = fs::metadata(&file.path()) {
+        data.push(FileEntry {
+            name: file
+                .file_name()
+                .into_string()
+                .unwrap_or("unknown name".into()),
+            e_type: if meta.is_dir() {
+                EntryType::Dir
+            } else {
+                EntryType::File
+            },
+            len_bytes: meta.len(),
+            modified: "".to_string(),
+        })
+    }
 }
