009bb1ff18ac4cbbfcf2d8643b7620f691ac092e
lapointek <lapointek.dev@gmail.com>
2025-12-15 14:22:25 -0500

    feat: add struct and attributes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use clap::Parser;
+use std::path::PathBuf;
+
+#[derive(Debug, Parser)]
+#[command(version)]
+struct Cli {
+    path: Option<PathBuf>,
+}
+
 fn main() {
+    let cli = Cli::parse();
     println!("Hello, world!");
 }
